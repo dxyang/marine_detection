b36e5c091b43313175dbe0ccd97f858d6c745163
Levi Cai <cail@mit.edu>
2024-04-05 20:19:58 -0400

    Added a util function to extract relevant files from a directory and maintains structure

---
+++ imerit/extract_image_dirs.py
@@ -0,0 +1,30 @@
+"""
+Finds all leaf files that match an extension and copies them to a new output directory with the same structure
+
+Usage:
+python copy_filetypes.py <input_dir> <output_dir> <filetype>
+
+Example:
+python copy_filetypes.py aws-fish yolo-fish png
+"""
+
+import glob
+import sys
+from pathlib import Path
+from os import makedirs
+from os.path import isdir
+import os
+
+import shutil
+
+root_dir = Path(sys.argv[1])
+output_dir = Path(sys.argv[2])
+filetype = sys.argv[3]
+
+filepaths = glob.iglob(str(root_dir / f"**/*.{filetype}"), recursive=True)
+
+for filepath in filepaths:
+    print(filepath)
+    output_path = Path(output_dir, *Path(filepath).parts[1:])
+    os.makedirs(str(output_path.parent), exist_ok=True)
+    shutil.copyfile(filepath, output_path)
